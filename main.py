44fad3e474964e4cfbcc1e4ca8fa1da9b3a43c15
Dan Shevel <dan.shevel@trywonder.ai>
2025-04-18 12:40:05 +0300

    Brought my Game of Life from leetcode, and adjusted it a bit to set grid size

---
+++ main.py
@@ -0,0 +1,50 @@
+from typing import List
+from random import randint
+
+CURRENT_SIZE = 4
+
+grid = [[0 for _ in range(CURRENT_SIZE)] for _ in range(CURRENT_SIZE)]
+
+print(grid)
+
+def randomize_grid(grid: List[List[int]]) -> List[List[int]]:
+    return [[randint(0,1) for _ in range(len(grid))] for _ in range(len(grid))]
+
+def count_neighbors(grid: List[List[int]], x: int, y: int) -> int:
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            nx, ny = x + i, y + j
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid):
+                count += grid[nx][ny]
+    return count
+
+def next_step(grid: List[List[int]]) -> List[List[int]]:
+    new_grid = [[0 for _ in range(len(grid))] for _ in range(len(grid))]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            neighbors = count_neighbors(grid, i, j)
+            if grid[i][j] == 1:
+                if neighbors < 2 or neighbors > 3:
+                    new_grid[i][j] = 0
+                else:
+                    new_grid[i][j] = 1
+            else:
+                if neighbors == 3:
+                    new_grid[i][j] = 1
+    return new_grid
+
+
+def print_grid(grid: List[List[int]]) -> None:
+    print(' ')
+    for i in range(len(grid)):
+        print(grid[i])
+grid = randomize_grid(grid)
+print(f"Randomize grid:")
+print_grid(grid)
+for _ in range(15):
+    grid = next_step(grid)
+print(f"Next step:")
+print_grid(grid)
